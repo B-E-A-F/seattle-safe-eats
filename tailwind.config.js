a9fdde15902acdac3cd3c54954930e8e8873b7f6
ArcherScript <mikearche@gmail.com>
2024-05-27 15:53:29 -0700

    ✨  Add DaisyUI and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [daisyui]
+};
