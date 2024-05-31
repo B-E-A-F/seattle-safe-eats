b003cff473b90f842708ae672d23189c0db8e219
ArcherScript <mikearche@gmail.com>
2024-05-31 10:07:16 -0700

    Add missing title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import Search from '$lib/components/Search.svelte';
 </script>
 
+<svelte:head>
+	<title>Seattle Safe Eats</title>
+</svelte:head>
+
 <div class="flex flex-1 justify-center items-center flex-col gap-6">
 	<h1 class="text-6xl text-center">Seattle<br />Safe<br />Eats</h1>
 	<Search />
