b0d206db2ea8b2266f6728952d6fd848a4c50451
ArcherScript <mikearche@gmail.com>
2024-06-04 20:14:01 -0700

    Fix ios search bug

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -16,7 +16,7 @@
 	});
 </script>
 
-<form class={className} on:submit|preventDefault={navigateSearch}>
+<form class={className} on:submit|preventDefault={navigateSearch} action=".">
 	<label class="input input-bordered flex items-center gap-2">
 		<input type="search" class="grow" placeholder="Search for a restaurant" bind:value={search} />
 		<MagnifyingGlass />
