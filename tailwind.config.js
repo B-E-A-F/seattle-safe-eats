539b1c5610f7c4f787f54bb9993738ae2d87c19f
Nathan Roark <nathanroark@protonmail.com>
2024-08-11 23:20:09 -0500

    flash to coffee first before using given theme from cookie

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import daisyui from 'daisyui';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	daisyui: {
-		themes: true
+		themes: ['coffee', 'cupcake']
 	},
 	theme: {
 		container: {
