5b486d5ac9b7ef9a14791b6d185f56059159f9bd
bea <bea@a4r.dev>
2024-11-28 20:21:09 -0800

    🐛 Fix reverse compatibility issues with urls

--- src/routes/restaurant/[id]/+page.ts
+++ src/routes/restaurant/[id]/+page.ts
@@ -2,8 +2,15 @@ import type { Business } from '../lookup/[id]/+server';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async function ({ fetch, params }) {
-	const idParts = params.id.split('-');
-	const id = 'PR' + idParts[idParts.length - 1];
+	// TODO: remove this once google has indexed the new urls
+	let id;
+
+	if (params.id.substring(0, 2) === 'PR') {
+		id = params.id;
+	} else {
+		const idParts = params.id.split('-');
+		id = 'PR' + idParts[idParts.length - 1];
+	}
 
 	const url = `/restaurant/lookup/${id}`;
 
